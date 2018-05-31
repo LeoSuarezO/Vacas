6be3bae5ccbeab3a305800729041fd1996884b21
Andrés Felipe Chaparro <andres.chaparro06@uptc.edu.co>
2018-05-30 22:36:32 -0500

    Agregada funcion de cerrar loginjdialog

--- Vacas/src/com/uptc/livestock/controller/LivestockListener.java
+++ Vacas/src/com/uptc/livestock/controller/LivestockListener.java
@@ -3,14 +3,23 @@ package com.uptc.livestock.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import com.uptc.livestock.model.entity.Command;
+import com.uptc.livestock.view.LogInJDialog;
+
 public class LivestockListener implements ActionListener {
 	private static LivestockListener livestockListener;
+	private LogInJDialog logInJDialog;
 
 	private LivestockListener() {
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		switch (Command.valueOf(e.getActionCommand())) {
+		case DISPOSE_LOGIN_JDIALOG:
+			logInJDialog.dispose();
+			break;
+		}
 	}
 
 	public static LivestockListener getIntance() {
@@ -19,4 +28,9 @@ public class LivestockListener implements ActionListener {
 		}
 		return livestockListener;
 	}
+
+	public void setLogInJDialog(LogInJDialog logInJDialog) {
+		this.logInJDialog = logInJDialog;
+	}
 }
+
