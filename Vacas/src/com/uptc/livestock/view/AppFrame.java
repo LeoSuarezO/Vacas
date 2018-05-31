fcc8d32bcec12f8bf52cdfe6b84ef8750835500c
Andrés Felipe Chaparro <andres.chaparro06@uptc.edu.co>
2018-05-30 22:36:11 -0500

    centrar frame principal

--- Vacas/src/com/uptc/livestock/view/AppFrame.java
+++ Vacas/src/com/uptc/livestock/view/AppFrame.java
@@ -4,10 +4,12 @@ import javax.swing.JFrame;
 
 public class AppFrame extends JFrame {
 	public AppFrame() {
+		new LogInJDialog();
 		init();
 	}
 
 	private void init() {
+		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setVisible(true);
 	}
