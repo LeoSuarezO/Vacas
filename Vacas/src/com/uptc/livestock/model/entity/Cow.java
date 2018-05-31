bb1b14c2312db3e90b8ee5b45e7df6d2d6d694d7
Andrés Felipe Chaparro <andres.chaparro06@uptc.edu.co>
2018-05-30 19:38:09 -0500

    Creado constructor con un vector como parametro

--- Vacas/src/com/uptc/livestock/model/entity/Cow.java
+++ Vacas/src/com/uptc/livestock/model/entity/Cow.java
@@ -1,13 +1,20 @@
 package com.uptc.livestock.model.entity;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Cow extends Bovine{
-	
-//	private double cantMilkProduce;
+import com.uptc.livestock.utilities.Utilities;
+
+public class Cow extends Bovine {
 
 	public Cow(short id, String name, Date birthDate, Race race, double weight, String vaccine,
 			HealthCondition healthCondition) {
 		super(id, name, birthDate, race, weight, vaccine, healthCondition);
+	}
+
+	public Cow(String[] data) {
+		this(Short.parseShort(data[0]), data[1], Utilities.parseDate(data[2]), Race.valueOf(data[3]),
+				Double.parseDouble(data[4]), data[5], HealthCondition.valueOf(data[6]));
 	}
 }
