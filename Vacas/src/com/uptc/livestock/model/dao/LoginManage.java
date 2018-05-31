83794fc2c3c28f284647753f1d30471705481474
Andrés Felipe Chaparro <andres.chaparro06@uptc.edu.co>
2018-05-30 19:22:01 -0500

    Generador de nombres de usuario

--- Vacas/src/com/uptc/livestock/model/dao/LoginManage.java
+++ Vacas/src/com/uptc/livestock/model/dao/LoginManage.java
@@ -1,5 +1,32 @@
 package com.uptc.livestock.model.dao;
 
-public class LoginManage {
+import java.util.HashMap;
+import java.util.Map;
 
+public class LoginManage {
+	private Map<String, String> userNamePassword;
+
+	public LoginManage() {
+		userNamePassword = new HashMap<>();
+	}
+
+	public String generateUsername(String userFirstName, String userLastName, String password) {
+		String username = useName(userFirstName, userLastName);
+		userNamePassword.put(username, password);
+		return username;
+	}
+
+	public String useName(String userFirstName, String userLastName) {
+		String username = buildUsername(userFirstName,userLastName);
+		int counter = 1;
+		while (userNamePassword.keySet().contains(username)) {
+			username = buildUsername(userFirstName,userLastName) + 00 + counter;
+			counter++;
+		}
+		return username;
+	}
+
+	private String buildUsername(String userFirstName, String userLastName) {
+		return (userFirstName + '.' + userLastName).toLowerCase();
+	}
 }
