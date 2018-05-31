dfc856d38d411a6c55d1f25cf649058fff1da1bb
usuario <usuario@DESKTOP-65CLIS4>
2018-05-31 16:59:49 -0500

    Creacion de clase Rancher V1

---
+++ Vacas/src/com/uptc/livestock/model/entity/Rancher.java
@@ -0,0 +1,58 @@
+package com.uptc.livestock.model.entity;
+
+public class Rancher {
+	
+	private short id;
+	private String name;
+	private String lastName;
+	private String address;
+	private int phoneNumber;
+	
+	public Rancher(short id, String name, String lastName, String address, int phoneNumber) {
+		this.id = id;
+		this.name = name;
+		this.lastName = lastName;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public short getId() {
+		return id;
+	}
+
+	public void setId(short id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+}
