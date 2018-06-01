0df2f23c4f0fe14d9fb034fd14eacb1638dabc50
Andrés Felipe Chaparro <andres.chaparro06@uptc.edu.co>
2018-06-01 12:02:26 -0500

    Actualizado con encriptacion

--- Vacas/src/Test/TestUsername.java
+++ Vacas/src/Test/TestUsername.java
@@ -1,13 +1,45 @@
 package Test;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import com.uptc.livestock.model.dao.LoginManage;
+import com.uptc.livestock.model.entity.Rancher;
+import com.uptc.livestock.utilities.PasswordUtil;
 
 public class TestUsername {
-	public static void main(String[] args) {
-		LoginManage loginManage = new LoginManage();
-		System.out.println(loginManage.generateUsername("lola", "labandida", "00000000"));
-		System.out.println(loginManage.generateUsername("lola", "labandida", "00000000"));
-		System.out.println(loginManage.generateUsername("lola", "labandida", "00000000"));
-		System.out.println(loginManage.generateUsername("luis", "elbandido", "00000000"));
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		LoginManage lm = new LoginManage();
+		ArrayList<Rancher> ranchers = new ArrayList<>();
+		int option = -1;
+		do {
+			System.out.print("Nombre : ");
+			String name = sc.nextLine();
+			System.out.print("Apellido : ");
+			String lastname = sc.nextLine();
+			System.out.print("Usuario : ");
+			System.out.println(lm.useName(name, lastname));
+			String password;
+			String password2;
+
+			do {
+				System.out.print("Contraseña : ");
+				password = sc.nextLine();
+				System.out.print("Confirmar contraseña : ");
+				password2 = sc.nextLine();
+				if (!password.equals(password2))
+					System.out.println("Las contraseñas no coinciden");
+			} while (!password.equals(password2));
+			System.out.println("Contraseña admitida");
+			ranchers.add(new Rancher((short) 0, name, lastname, "ss", 00, lm.useName(name, lastname)));
+			lm.generateUsername(name, lastname, PasswordUtil.getHash(password));
+
+			System.out.print("salir? [1]");
+			option = Integer.parseInt(sc.nextLine());
+		} while (option != 1);
+		for (Rancher rancher : ranchers) {
+			System.out.println(rancher.getUsername());
+		}
 	}
 }
--- Vacas/src/com/uptc/livestock/model/dao/LoginManage.java
+++ Vacas/src/com/uptc/livestock/model/dao/LoginManage.java
@@ -1,29 +1,45 @@
 package com.uptc.livestock.model.dao;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
+
+import com.uptc.livestock.model.entity.HashSalt;
+import com.uptc.livestock.utilities.PasswordUtil;
 
 public class LoginManage {
-	private Map<String, String> userNamePassword;
+	private Map<String, HashSalt> userNamePassword;
 
 	public LoginManage() {
 		userNamePassword = new HashMap<>();
 	}
 
-	public String generateUsername(String userFirstName, String userLastName, String password) {
+	public String generateUsername(String userFirstName, String userLastName, HashSalt hashSalt) {
 		String username = useName(userFirstName, userLastName);
-		userNamePassword.put(username, password);
+		userNamePassword.put(username, hashSalt);
 		return username;
 	}
 
 	public String useName(String userFirstName, String userLastName) {
-		String username = buildUsername(userFirstName,userLastName);
+		String username = buildUsername(userFirstName, userLastName);
 		int counter = 1;
 		while (userNamePassword.keySet().contains(username)) {
-			username = buildUsername(userFirstName,userLastName) + 00 + counter;
+			username = buildUsername(userFirstName, userLastName) + 00 + counter;
 			counter++;
 		}
 		return username;
+	}
+
+	public boolean existUser(String username, String password) {
+		Iterator<Entry<String, HashSalt>> it = userNamePassword.entrySet().iterator();
+		while (it.hasNext()) {
+			Entry<String, HashSalt> e = it.next();
+			if(e.getKey().equals(username))
+				if(PasswordUtil.ValidatePass(password, e.getValue().getHash(), e.getValue().getSalt()))
+					return true;
+		}
+		return false;
 	}
 
 	private String buildUsername(String userFirstName, String userLastName) {
