b200468d41a33daf3959d64baf687763c78c36d8
Andrés Felipe Chaparro <andres.chaparro06@uptc.edu.co>
2018-06-01 16:59:17 -0500

    test de panel bienvenida

---
+++ Vacas/src/Test/TestPresentationJPanel.java
@@ -0,0 +1,16 @@
+package Test;
+
+import javax.swing.JFrame;
+
+import com.uptc.livestock.view.PresentationJPanel;
+
+public class TestPresentationJPanel {
+
+	public static void main(String[] args) {
+		JFrame f = new JFrame("Test presentacion");
+		f.add(new PresentationJPanel());
+		f.setLocationRelativeTo(null);
+		f.setSize(640, 480);
+		f.setVisible(true);
+	}
+}
