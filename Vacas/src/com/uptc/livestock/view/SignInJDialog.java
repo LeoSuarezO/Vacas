ba5f0cd49d662dabb8da0fef635e81e2c17574a5
Andrés Felipe Chaparro <andres.chaparro06@uptc.edu.co>
2018-06-01 15:42:56 -0500

    mejora de interfaz

--- Vacas/src/com/uptc/livestock/view/SignInJDialog.java
+++ Vacas/src/com/uptc/livestock/view/SignInJDialog.java
@@ -9,11 +9,17 @@ import javax.swing.border.EmptyBorder;
 
 import com.uptc.livestock.controller.Command;
 import com.uptc.livestock.controller.LivestockListener;
+import com.uptc.livestock.controller.PasswordListener;
+import com.uptc.livestock.model.dao.LoginManage;
 
 import java.awt.GridBagLayout;
 import javax.swing.JLabel;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.Font;
 import javax.swing.SwingConstants;
 import java.awt.Color;
@@ -22,8 +28,9 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
 public class SignInJDialog extends JDialog {
-	private HintJTextField lastNameField;
 	private HintJTextField firstNameField;
+	private HintJTextField lastNameField;
+	private JLabel UsernameLabel;
 	private HintJTextField addressField;
 	private HintJTextField phoneField;
 	private HintJPasswordField passwordField;
@@ -49,12 +56,13 @@ public class SignInJDialog extends JDialog {
 		passwordField = new HintJPasswordField();
 		passwordField.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		passwordField.setHorizontalAlignment(SwingConstants.CENTER);
+		passwordField.addKeyListener(PasswordListener.getInstance());
 
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		GridBagLayout gb_contentpanel = new GridBagLayout();
 		gb_contentpanel.columnWeights = new double[] { 0.0, 0.0 };
-		gb_contentpanel.rowHeights = new int[] { 37, 50, 50, 50, 34 };
+		gb_contentpanel.rowHeights = new int[] { 37, 50, 0, 50, 50, 34 };
 		gb_contentpanel.columnWidths = new int[] { 245, 215 };
 		contentPanel.setLayout(gb_contentpanel);
 
@@ -82,6 +90,18 @@ public class SignInJDialog extends JDialog {
 		lastNameField = new HintJTextField();
 		lastNameField.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		lastNameField.setHorizontalAlignment(SwingConstants.CENTER);
+		lastNameField.addFocusListener(new FocusListener() {
+
+			@Override
+			public void focusLost(FocusEvent e) {
+				UsernameLabel.setText(LoginManage.useName(firstNameField.getText(), lastNameField.getText()));
+			}
+
+			@Override
+			public void focusGained(FocusEvent e) {
+			}
+		});
+		lastNameField.setActionCommand(Command.CONFIRM_FIELD.toString());
 
 		GridBagConstraints gbc_usernameField = new GridBagConstraints();
 		gbc_usernameField.insets = new Insets(0, 0, 5, 0);
@@ -90,6 +110,17 @@ public class SignInJDialog extends JDialog {
 		gbc_usernameField.gridy = 1;
 		contentPanel.add(lastNameField, gbc_usernameField);
 
+		UsernameLabel = new JLabel();
+		UsernameLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		UsernameLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		UsernameLabel.setForeground(Color.WHITE);
+		GridBagConstraints gbc_lblUsernamelabel = new GridBagConstraints();
+		gbc_lblUsernamelabel.gridwidth = 2;
+		gbc_lblUsernamelabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblUsernamelabel.gridx = 0;
+		gbc_lblUsernamelabel.gridy = 2;
+		contentPanel.add(UsernameLabel, gbc_lblUsernamelabel);
+
 		addressField = new HintJTextField();
 		addressField.setHorizontalAlignment(SwingConstants.CENTER);
 		addressField.setFont(new Font("Tahoma", Font.PLAIN, 16));
@@ -97,7 +128,7 @@ public class SignInJDialog extends JDialog {
 		gbc_addressField.insets = new Insets(0, 0, 5, 5);
 		gbc_addressField.fill = GridBagConstraints.BOTH;
 		gbc_addressField.gridx = 0;
-		gbc_addressField.gridy = 2;
+		gbc_addressField.gridy = 3;
 		contentPanel.add(addressField, gbc_addressField);
 
 		phoneField = new HintJTextField();
@@ -107,14 +138,14 @@ public class SignInJDialog extends JDialog {
 		gbc_phoneField.insets = new Insets(0, 0, 5, 0);
 		gbc_phoneField.fill = GridBagConstraints.BOTH;
 		gbc_phoneField.gridx = 1;
-		gbc_phoneField.gridy = 2;
+		gbc_phoneField.gridy = 3;
 		contentPanel.add(phoneField, gbc_phoneField);
 
 		GridBagConstraints gbc_passwordField = new GridBagConstraints();
 		gbc_passwordField.insets = new Insets(0, 0, 5, 5);
 		gbc_passwordField.fill = GridBagConstraints.BOTH;
 		gbc_passwordField.gridx = 0;
-		gbc_passwordField.gridy = 3;
+		gbc_passwordField.gridy = 4;
 		contentPanel.add(passwordField, gbc_passwordField);
 
 		signInButton = new JButton();
@@ -124,22 +155,24 @@ public class SignInJDialog extends JDialog {
 		confirmPasswordField = new HintJPasswordField();
 		confirmPasswordField.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		confirmPasswordField.setHorizontalAlignment(SwingConstants.CENTER);
+		confirmPasswordField.addKeyListener(PasswordListener.getInstance());
 		GridBagConstraints gbc_confirmPasswordField = new GridBagConstraints();
 		gbc_confirmPasswordField.fill = GridBagConstraints.BOTH;
 		gbc_confirmPasswordField.insets = new Insets(0, 0, 5, 0);
 		gbc_confirmPasswordField.gridx = 1;
-		gbc_confirmPasswordField.gridy = 3;
+		gbc_confirmPasswordField.gridy = 4;
 		contentPanel.add(confirmPasswordField, gbc_confirmPasswordField);
 		signInButton.setActionCommand(Command.SIGNIN_ACCOUNT.toString());
 		signInButton.setBorderPainted(false);
 		signInButton.setForeground(new Color(100, 149, 237));
 		signInButton.setBackground(Color.WHITE);
+		signInButton.setEnabled(false);
 
 		GridBagConstraints gbc_loginButton = new GridBagConstraints();
 		gbc_loginButton.gridwidth = 2;
 		gbc_loginButton.fill = GridBagConstraints.VERTICAL;
 		gbc_loginButton.gridx = 0;
-		gbc_loginButton.gridy = 4;
+		gbc_loginButton.gridy = 5;
 		contentPanel.add(signInButton, gbc_loginButton);
 
 		getContentPane().add(contentPanel);
@@ -171,6 +204,10 @@ public class SignInJDialog extends JDialog {
 		logInButton.setForeground(Color.WHITE);
 		logInButton.setBorder(null);
 
+		PasswordListener.getInstance().setSignInButton(signInButton);
+		PasswordListener.getInstance().setPasswordField(passwordField);
+		PasswordListener.getInstance().setConfirmPasswordField(confirmPasswordField);
+		
 		optionPanel.add(logInButton);
 
 		setLocationRelativeTo(null);
@@ -178,7 +215,8 @@ public class SignInJDialog extends JDialog {
 	}
 
 	public String[] getUserData() {
-		return new String[] { firstNameField.getText(), lastNameField.getText(), passwordField.getText() };
+		return new String[] { firstNameField.getText(), lastNameField.getText(), addressField.getText(),
+				phoneField.getText(), passwordField.getText() };
 	}
 
 	public void setStringsLanguage(String firstNameHint, String lastNameHint, String addressHint, String phoneHint,
@@ -194,4 +232,5 @@ public class SignInJDialog extends JDialog {
 		this.logInButton.setText(loginButton);
 		this.signInButton.setText(signinButton);
 	}
+
 }
