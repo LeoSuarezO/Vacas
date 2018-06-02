8a67c0caf92b807f46de1fcdbe62e7915b3d7b28
Andrés Felipe Chaparro <andres.chaparro06@uptc.edu.co>
2018-06-01 21:58:07 -0500

    Listener para el combobox de lenguajes

---
+++ Vacas/src/com/uptc/livestock/controller/LanguageListener.java
@@ -0,0 +1,38 @@
+package com.uptc.livestock.controller;
+
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import com.uptc.livestock.view.Language;
+
+public class LanguageListener implements ItemListener{
+	private static LanguageListener LanguageListener = null;
+	
+	private LanguageListener() {
+	}
+	
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		switch(Language.valueOf(Language.valueOfString(e.getItem().toString()).toString())) {
+		case ESPANIOL:
+			ChangeLanguage.getInstance().setLanguage(Language.ESPANIOL);
+			break;
+		case DEUTSCH:
+			ChangeLanguage.getInstance().setLanguage(Language.DEUTSCH);
+			break;
+		case ENGLISH:
+			ChangeLanguage.getInstance().setLanguage(Language.ENGLISH);
+			break;
+		default:
+			System.out.println("Este lenguaje " + Language.valueOfString(e.getItem().toString()).toString() + " no tiene funcion aun");
+			break;
+		}
+	}
+	
+	public static LanguageListener getInstance() {
+		if(LanguageListener==null) {
+			LanguageListener=new LanguageListener();
+		}
+		return LanguageListener;
+	}
+}
