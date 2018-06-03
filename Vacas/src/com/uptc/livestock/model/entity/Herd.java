b8d042aa494f50910eac99cec6374959c33689f6
Andrés Felipe Chaparro <andres.chaparro06@uptc.edu.co>
2018-06-03 01:37:34 -0500

    Clase herd contiene varios bovinos, ganadero con varios herd

---
+++ Vacas/src/com/uptc/livestock/model/entity/Herd.java
@@ -0,0 +1,11 @@
+package com.uptc.livestock.model.entity;
+
+import java.util.ArrayList;
+
+public class Herd {
+	private ArrayList<Bovine> bovines;
+
+	public Herd() {
+		bovines = new ArrayList<>();
+	}
+}
