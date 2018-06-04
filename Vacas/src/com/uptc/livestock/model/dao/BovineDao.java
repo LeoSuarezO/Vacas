181f7e45a5f98b890bc449fdb2ccdf8dcc0258bb
Andrés Felipe Chaparro <andres.chaparro06@uptc.edu.co>
2018-06-04 12:34:00 -0500

    Agregadas clases Dao de Bovinos y hatos

---
+++ Vacas/src/com/uptc/livestock/model/dao/BovineDao.java
@@ -0,0 +1,34 @@
+package com.uptc.livestock.model.dao;
+
+import java.util.ArrayList;
+
+import com.uptc.livestock.model.entity.Bovine;
+
+public class BovineDao {
+	private ArrayList<Bovine> bovines;
+	
+	public BovineDao() {
+		bovines = new ArrayList<>();
+	}
+	
+	public void createBovine() {
+		
+	}
+	
+	public void readBovine() {
+		
+	}
+	
+	public void updateBovine() {
+	
+	}
+	
+	public void deleteBovine() {
+		
+	}
+	
+	public boolean isEmpty() {
+		return bovines.isEmpty();
+	}
+
+}
---
+++ Vacas/src/com/uptc/livestock/model/dao/HerdDao.java
@@ -0,0 +1,35 @@
+package com.uptc.livestock.model.dao;
+
+import java.util.ArrayList;
+
+import com.uptc.livestock.model.entity.Herd;
+
+public class HerdDao {
+
+	private ArrayList<Herd> herds;
+	
+	public HerdDao() {
+		this.herds= new ArrayList<>();
+	}
+	
+	public void createHerd() {
+		
+	}
+	
+	public void readHerd() {
+		
+	}
+	
+	public void updateHerd() {
+		
+	}
+	
+	public void deleteHerd() {
+		
+	}
+	
+	public boolean isEmpty() {
+		return herds.isEmpty();
+	}
+	
+}
