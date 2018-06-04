bf9615d7e589d0df4c050ad81ddd7fdd44a8919b
Andrés Felipe Chaparro <andres.chaparro06@uptc.edu.co>
2018-06-04 00:40:55 -0500

    Panel principal

---
+++ Vacas/src/com/uptc/livestock/view/MainJPanel.java
@@ -0,0 +1,25 @@
+package com.uptc.livestock.view;
+
+import javax.swing.JPanel;
+
+import com.uptc.livestock.controller.LivestockListener;
+import com.uptc.livestock.model.entity.Herd;
+import com.uptc.livestock.model.entity.Rancher;
+
+public class MainJPanel extends JPanel{
+	private Rancher rancher;
+	public MainJPanel() {
+		init();
+	}
+	private void init() {
+		LivestockListener.getIntance().setMainJPanel(this);
+	}
+	
+	public void setRancher(Rancher rancher) {
+		this.rancher = rancher;
+		if(rancher.getHerds().isEmpty())
+			System.out.println("Vacio");
+		else
+			System.out.println("No vacio");
+	}
+}
